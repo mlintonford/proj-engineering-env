1ce848cb7063f7418fb0bf7f890b0923633703d5
mlintonford <mlinton_ford@hotmail.com>
2016-02-22 13:32:41 +0000

    avoiding the mess with infrastructure vs Infrastructure

---
+++ Infrastructure/VanillaVM/provision.ps1
@@ -0,0 +1,114 @@
+$ErrorActionPreference = "Stop"
+
+Write-Host "Enabling file sharing firewale rules"
+netsh advfirewall firewall set rule group="File and Printer Sharing" new enable=yes
+
+if(Test-Path "C:\Users\vagrant\VBoxGuestAdditions.iso") {
+    Write-Host "Installing Guest Additions"
+    certutil -addstore -f "TrustedPublisher" A:\oracle.cer
+    cinst 7zip.commandline -y
+    Move-Item C:\Users\vagrant\VBoxGuestAdditions.iso C:\Windows\Temp
+    7z x C:\Windows\Temp\VBoxGuestAdditions.iso -oC:\Windows\Temp\virtualbox
+
+    Start-Process -FilePath "C:\Windows\Temp\virtualbox\VBoxWindowsAdditions.exe" -ArgumentList "/S" -WorkingDirectory "C:\Windows\Temp\virtualbox" -Wait
+
+    Remove-Item C:\Windows\Temp\virtualbox -Recurse -Force
+    Remove-Item C:\Windows\Temp\VBoxGuestAdditions.iso -Force
+}
+
+Write-Host "Cleaning SxS..."
+Dism.exe /online /Cleanup-Image /StartComponentCleanup /ResetBase
+
+@(
+    "$env:localappdata\Nuget",
+    "$env:localappdata\temp\*", 
+    "$env:windir\logs",
+    "$env:windir\panther",
+    "$env:windir\temp\*",
+    "$env:windir\winsxs\manifestcache"
+) | % {
+        if(Test-Path $_) {
+            Write-Host "Removing $_"
+            try {
+              Takeown /d Y /R /f $_
+              Icacls $_ /GRANT:r administrators:F /T /c /q  2>&1 | Out-Null
+              Remove-Item $_ -Recurse -Force | Out-Null 
+            } catch { $global:error.RemoveAt(0) }
+        }
+    }
+
+Write-Host "defragging..."
+if (Test-Command -cmdname 'Optimize-Volume') {
+    Optimize-Volume -DriveLetter C
+    } else {
+    Defrag.exe c: /H
+}
+
+Write-Host "0ing out empty space..."
+$FilePath="c:\zero.tmp"
+$Volume = Get-WmiObject win32_logicaldisk -filter "DeviceID='C:'"
+$ArraySize= 64kb
+$SpaceToLeave= $Volume.Size * 0.05
+$FileSize= $Volume.FreeSpace - $SpacetoLeave
+$ZeroArray= new-object byte[]($ArraySize)
+ 
+$Stream= [io.File]::OpenWrite($FilePath)
+try {
+   $CurFileSize = 0
+    while($CurFileSize -lt $FileSize) {
+        $Stream.Write($ZeroArray,0, $ZeroArray.Length)
+        $CurFileSize +=$ZeroArray.Length
+    }
+}
+finally {
+    if($Stream) {
+        $Stream.Close()
+    }
+}
+ 
+Del $FilePath
+
+Enable-RemoteDesktop
+netsh advfirewall firewall add rule name="Remote Desktop" dir=in localport=3389 protocol=TCP action=allow
+
+Update-ExecutionPolicy -Policy Unrestricted
+
+if (Test-Command -cmdname 'Uninstall-WindowsFeature') {
+    Remove-WindowsFeature -Name 'Powershell-ISE'
+    Get-WindowsFeature | 
+    ? { $_.InstallState -eq 'Available' } | 
+    Uninstall-WindowsFeature -Remove
+}
+
+
+Install-WindowsUpdate -AcceptEula
+
+$pageFileMemoryKey = "HKLM:\SYSTEM\CurrentControlSet\Control\Session Manager\Memory Management"
+Set-ItemProperty -Path $pageFileMemoryKey -Name PagingFiles -Value ""
+
+if(Test-PendingReboot){ Invoke-Reboot }
+
+netsh advfirewall firewall add rule name="WinRM-HTTP" dir=in localport=5985 protocol=TCP action=allow
+
+$enableArgs=@{Force=$true}
+try {
+ $command=Get-Command Enable-PSRemoting
+  if($command.Parameters.Keys -contains "skipnetworkprofilecheck"){
+      $enableArgs.skipnetworkprofilecheck=$true
+  }
+}
+catch {
+  $global:error.RemoveAt(0)
+}
+Enable-PSRemoting @enableArgs
+Enable-WSManCredSSP -Force -Role Server
+winrm set winrm/config/client/auth '@{Basic="true"}'
+winrm set winrm/config/service/auth '@{Basic="true"}'
+winrm set winrm/config/service '@{AllowUnencrypted="true"}'
+
+Write-Host "Recreate pagefile after sysprep"
+$System = GWMI Win32_ComputerSystem -EnableAllPrivileges
+if ($system -ne $null) {
+  $System.AutomaticManagedPagefile = $true
+  $System.Put()
+}

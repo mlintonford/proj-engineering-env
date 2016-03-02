244268d64af71ca76fc90f5656f0f199300f043e
mlintonford <matthew@mlfvs.info>
2016-03-02 09:26:03 +0000

    Removed vmware-tools installation during packer build

--- Infrastructure/VanillaVM/install-vmware-tools.ps1
+++
@@ -1,7 +0,0 @@
-$exe_name = 'VMware-tools-9.10.5-2981885-x86_64.exe'
-$download_url = "https://packages.vmware.com/tools/esx/6.0u1/windows/x64/"
-
-(New-Object System.Net.WebClient).DownloadFile($download_url, "c:\\windows\\temp\\setup.exe")
-&c:\windows\temp\setup.exe /S /v`"/qn REBOOT=R`" | Out-Host
-
-exit 0

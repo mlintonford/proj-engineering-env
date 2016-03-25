e1a61d2c2abfd1663923c9fc83cfa18ad61fe5c5
mlintonford <matthew@mlfvs.info>
2016-03-25 19:47:17 +0000

    known building vmware_Ubuntu_1404_x64, vagrant-upping GitlabServer, known issue - ks.cfg location not supported in GitlabServer

--- Infrastructure/VanillaVM/terraform/main.tf
+++ Infrastructure/VanillaVM/terraform/main.tf
@@ -1,27 +1,26 @@
 # Configure the VMware vSphere Provider
 provider "vsphere" {
-  user           = "${var.vsphere_user}"
-  password       = "${var.vsphere_password}"
-  vsphere_server = "${var.vsphere_server}"
-}
-
-# Create a folder
-resource "vsphere_folder" "frontend" {
-  path = "frontend"
+  user           = "root"
+  password       = "Telent2016"
+  vsphere_server = "137.221.108.88"
+  allow_unverified_ssl = true
 }
 
 # Create a virtual machine within the folder
-resource "vsphere_virtual_machine" "web" {
-  name   = "terraform_web"
-  folder = "${vsphere_folder.frontend.path}"
+resource "vsphere_virtual_machine" "scaa-01" {
+  name   = "dmmc-scaa-01"
   vcpu   = 2
   memory = 4096
 
   network_interface {
     label = "VM Network"
+	ipv4_address = "10.250.15.10"
   }
-
+  
   disk {
-    template = "mmlfvs/VanillaVM"
+    size = 40
+	template = "hashicorp/logstream"
+	datastore = "datastore1"
   }
+  
 }
